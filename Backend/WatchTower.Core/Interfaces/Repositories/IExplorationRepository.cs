e57b5de90b8a015818a1c5e2b54450ddb5d3e907
JosuGuzman <josu.guzmanet12d1@gmail.com>
2025-11-26 15:03:45 -0300

    Create IExplorationRepository.cs

---
+++ Backend/WatchTower.Core/Interfaces/Repositories/IExplorationRepository.cs
@@ -0,0 +1,9 @@
+namespace WatchTower.Core.Interfaces.Repositories;
+
+public interface IExplorationRepository
+{
+    Task<IEnumerable<ExplorationHistory>> GetUserHistoryAsync(int userId);
+    Task<bool> AddToHistoryAsync(ExplorationHistory history);
+    Task<bool> UpdateTimeSpentAsync(int historyId, int timeSpent);
+    Task<int> GetTotalExplorationTimeAsync(int userId);
+}
