c154b7e5e62eadaeffc829be2bc61b9f231f771d
JosuGuzman <josu.guzmanet12d1@gmail.com>
2025-11-26 15:07:32 -0300

    Create IDiscoveryService.cs

---
+++ Backend/WatchTower.Core/Interfaces/Services/IDiscoveryService.cs
@@ -0,0 +1,11 @@
+namespace WatchTower.Core.Interfaces.Services;
+
+public interface IDiscoveryService
+{
+    Task<DiscoveryDetailResponse?> GetByIdAsync(int id);
+    Task<PagedResult<DiscoveryResponse>> SearchAsync(DiscoverySearchRequest request);
+    Task<DiscoveryResponse> CreateAsync(DiscoveryCreateRequest request, int reportedBy);
+    Task<bool> VoteAsync(int discoveryId, int userId, VoteType voteType);
+    Task<bool> UpdateStatusAsync(int discoveryId, DiscoveryStatus status, int verifiedBy);
+    Task<IEnumerable<DiscoveryResponse>> GetUserDiscoveriesAsync(int userId);
+}
