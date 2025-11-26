6775c6564f762cfa80df35f3c01ba73c3d2bda93
JosuGuzman <josu.guzmanet12d1@gmail.com>
2025-11-26 15:03:47 -0300

    Create IFavoriteRepository.cs

---
+++ Backend/WatchTower.Core/Interfaces/Repositories/IFavoriteRepository.cs
@@ -0,0 +1,10 @@
+namespace WatchTower.Core.Interfaces.Repositories;
+
+public interface IFavoriteRepository
+{
+    Task<Favorite?> GetByIdAsync(int favoriteId);
+    Task<IEnumerable<Favorite>> GetUserFavoritesAsync(int userId);
+    Task<bool> AddFavoriteAsync(Favorite favorite);
+    Task<bool> RemoveFavoriteAsync(int favoriteId);
+    Task<bool> IsFavoriteAsync(int userId, int? celestialBodyId, int? articleId, int? discoveryId);
+}
