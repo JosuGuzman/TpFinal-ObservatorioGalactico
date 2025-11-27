1e7c78ae0f45ccca56ec6df67b5c1b40ae2a044d
JosuGuzman <josu.guzmanet12d1@gmail.com>
2025-11-27 14:01:29 -0300

    Create GlobalUsing.cs

---
+++ Backend/WatchTower.Infrastructure/GlobalUsing.cs
@@ -0,0 +1,6 @@
+global using Dapper;
+global using WatchTower.Core.Entities;
+global using WatchTower.Core.Enums;
+global using WatchTower.Core.Interfaces.Repositories;
+global using WatchTower.Infrastructure.Data;
+global using WatchTower.Core.DTOs;
