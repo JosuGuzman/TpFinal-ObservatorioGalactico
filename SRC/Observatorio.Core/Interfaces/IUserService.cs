fd35322263dd0d72670620e6a94e53d9868e7686
JosuGuzman <josu.guzmanet12d1@gmail.com>
2025-12-03 05:42:31 -0300

    Update IUserService.cs

--- SRC/Observatorio.Core/Interfaces/IUserService.cs
+++ SRC/Observatorio.Core/Interfaces/IUserService.cs
@@ -13,7 +13,7 @@ public interface IUserService
     Task GenerateApiKeyAsync(int userId);
     Task<bool> ValidateApiKeyAsync(string apiKey);
     Task<bool> UserExistsAsync(string email);
-    Task<IEnumerable<User>> GetAllUsersAsync();
     Task<int> GetTotalUsersCountAsync();
     Task<int> GetActiveUsersCountAsync();
+    Task<IEnumerable<User>> GetAllUsersAsync(int page = 1, int pageSize = 20);
 }
